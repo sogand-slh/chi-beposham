2e5fabae9adc31adbcebc82b999a74c69065f2fc
Sogand <sogandsaleh21@gmail.com>
2022-02-10 18:08:06 +0330

    add slider

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+//Add other component
+import HeroSlider from "../../components/HeroSlider";
 import HeroImage from "../../assets/images/hi-2.jpg";
 
 const Home = () => {
@@ -20,6 +22,7 @@ const Home = () => {
       <div className="w-full h-full p-2 flex justify-between items-center ">
         <img src={HeroImage} alt="" />
       </div>
+      <HeroSlider />
       <div>{"info"}</div>
       <div>{data?.[0]?.price}</div>
     </>
